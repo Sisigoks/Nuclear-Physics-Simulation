c02b45a287ae6ed1957ce83dfe6545e27f5dc1f6
Sisigoks <Sisi.goks2008@gmail.com>
2025-10-05 12:48:43 +0530

    chngaes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Nuclear-Physics-Simulation/',
   server: {
     port: 3000,
     open: true
